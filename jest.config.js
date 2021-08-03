b8d08669fafb68f6acea47d15706059287e85ab6
Sven Patrick Meier <sven.patrick.meier@team-parallax.com>
2021-08-03 13:30:58 +0200

    chore(jest): add test-setup to auto-scaler
    add scripts to auto-scaler package to enable testing
    
    re #31

---
+++ jest.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+	globals: {
+		"ts-jest": {
+			"diagnostics": false
+		}
+	},
+	moduleDirectories: [
+		"node_modules",
+		"src"
+	],
+	moduleFileExtensions: [
+		"ts",
+		"js",
+		"node"
+	],
+	moduleNameMapper: {
+		"^Controller(.*)$": "<rootDir>/src/controller$1",
+		"^Model(.*)$": "<rootDir>/src/model$1",
+		"^Services(.*)$": "<rootDir>/src/services$1",
+		"~/": "<rootDir>/src/"
+	},
+	setupFilesAfterEnv: [
+		"./jest.setup.ts"
+	],
+	testRegex: [
+		"(/__tests__/.*|\\.(test|spec)|\\.(unit|integration))\\.(ts|tsx|js)$"
+	],
+	transform: {
+		".ts$": "ts-jest"
+	},
+	transformIgnorePatterns: [
+		"!node_modules/"
+	],
+	verbose: true
+}
