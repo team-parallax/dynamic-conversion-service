a15b65a250c7ccb43790db82667000d241748694
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2021-08-06 11:47:35 +0200

    docs(auto-scaler): remove unnecessary jsdoc

--- packages/auto-scaler/src/config/interface.ts
+++ packages/auto-scaler/src/config/interface.ts
@@ -1,18 +1,6 @@
-/**
- * The Configuration of the AutoScaler.
- */
 export interface IAutoScalerConfiguration {
-    /**
-     * Threshold for starting containers.
-     */
     containerStartThreshold: number,
-    /**
-     * The docker configuration.
-     */
     dockerConfig: IDockerConfiguration,
-     /**
-      * Maximum number of containers which are allowed to run.
-      */
     maxContainers: number
 }
 export interface IDockerConfiguration {
