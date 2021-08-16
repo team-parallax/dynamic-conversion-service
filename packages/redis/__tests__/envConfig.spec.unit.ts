db9684c5513db2d88185904fc014e491ace71dd9
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2021-08-16 15:33:36 +0200

    test(redis-service): add more test cases

--- packages/redis/__tests__/envConfig.spec.unit.ts
+++ packages/redis/__tests__/envConfig.spec.unit.ts
@@ -1,10 +1,10 @@
-import { InvalidConfigurationError } from "../src/config/exception"
-import { getRedisConfigFromEnv } from "../src/config"
+import { InvalidConfigurationError, InvalidConfigurationValueError } from "../src/config/exception"
+import { getRedisConfigFromEnv, isNumber } from "../src/config"
 describe("loading configuration fields from environment should work", () => {
 	afterEach(() => {
 		delete process.env.TASKS_PER_CONTAINER
-		delete process.env.MAX_WORKER_CONTAINER
-		delete process.env.MIN_WORKER_CONTAINER
+		delete process.env.MAX_WORKER_CONTAINERS
+		delete process.env.MIN_WORKER_CONTAINERS
 		delete process.env.CONTAINER_LABEL
 		delete process.env.CONTAINER_IMAGE
 		delete process.env.CONTAINER_TAG
@@ -15,6 +15,20 @@ describe("loading configuration fields from environment should work", () => {
 		delete process.env.REDIS_PORT
 		delete process.env.REDIS_NS
 		delete process.env.REDIS_QUEUE
+		delete process.env.HEALTH_CHECK_INTERVAL
+		delete process.env.APPLY_DESIRED_STATE_INTERVAL
+	})
+	describe("number validation should work", () => {
+		it("should report numbers as valid", () => {
+			expect(isNumber("123")).toBe(true)
+			expect(isNumber("0")).toBe(true)
+			expect(isNumber("420691337")).toBe(true)
+		})
+		it("should report non-numbers as invalid", () => {
+			expect(isNumber("foo-bar")).toBe(false)
+			expect(isNumber("0kekw123")).toBe(false)
+			expect(isNumber("123_asasdposdkfg2134q23")).toBe(false)
+		})
 	})
 	describe("it should load valid configurations", () => {
 		it("should load a minimal valid configuration (docker socket)", () => {
@@ -28,38 +42,73 @@ describe("loading configuration fields from environment should work", () => {
 			process.env.REDIS_PORT = "6379"
 			process.env.REDIS_NS = "test-ns"
 			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
 			expect(() => getRedisConfigFromEnv()).not.toThrowError()
 		})
-		it("should load a minimal valid configuration (docker host/port)", () => {
+		it("should load a minimal valid configuration without optional fields", () => {
 			process.env.TASKS_PER_CONTAINER = "5"
 			process.env.MAX_WORKER_CONTAINERS = "10"
 			process.env.MIN_WORKER_CONTAINERS = "1"
 			process.env.CONTAINER_LABEL = "test-label"
 			process.env.IMAGE_NAME = "bash"
+			process.env.CONTAINER_TAG = "latest"
 			process.env.DOCKER_HOST = "127.0.0.1"
 			process.env.DOCKER_PORT = "1234"
 			process.env.REDIS_HOST = "127.0.0.1"
 			process.env.REDIS_PORT = "6379"
 			process.env.REDIS_NS = "test-ns"
 			process.env.REDIS_QUEUE = "test-queue"
+			// Process.env.HEALTH_CHECK_INTERVAL = "120"
+			// Process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
 			expect(() => getRedisConfigFromEnv()).not.toThrowError()
 		})
 	})
 	describe("it should handle invalid values for fields", () => {
 		it("should throw an error when loading a invalid number", () => {
 			process.env.TASKS_PER_CONTAINER = "foobar"
-			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationError)
-		})
-		it("should throw an error when loading a invalid number", () => {
-			process.env.TASKS_PER_CONTAINER = "5"
 			process.env.MAX_WORKER_CONTAINERS = "10"
 			process.env.MIN_WORKER_CONTAINERS = "1"
 			process.env.CONTAINER_LABEL = "test-label"
 			process.env.IMAGE_NAME = "bash"
 			process.env.DOCKER_SOCKET_PATH = "/var/run/docker.sock"
 			process.env.REDIS_HOST = "127.0.0.1"
-			process.env.REDIS_PORT = "foobar69420"
-			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationError)
+			process.env.REDIS_PORT = "6379"
+			process.env.REDIS_NS = "test-ns"
+			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
+			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationValueError)
+		})
+		it("should throw an error when loading a invalid number", () => {
+			process.env.TASKS_PER_CONTAINER = "foobar"
+			process.env.MAX_WORKER_CONTAINERS = "10"
+			process.env.MIN_WORKER_CONTAINERS = "1"
+			process.env.CONTAINER_LABEL = "test-label"
+			process.env.IMAGE_NAME = "bash"
+			process.env.DOCKER_SOCKET_PATH = "/var/run/docker.sock"
+			process.env.REDIS_HOST = "127.0.0.1"
+			process.env.REDIS_PORT = "69foobar420"
+			process.env.REDIS_NS = "test-ns"
+			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
+			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationValueError)
+		})
+		it("should throw an error when loading a optional invalid number", () => {
+			process.env.TASKS_PER_CONTAINER = "foobar"
+			process.env.MAX_WORKER_CONTAINERS = "10"
+			process.env.MIN_WORKER_CONTAINERS = "1"
+			process.env.CONTAINER_LABEL = "test-label"
+			process.env.IMAGE_NAME = "bash"
+			process.env.DOCKER_SOCKET_PATH = "/var/run/docker.sock"
+			process.env.REDIS_HOST = "127.0.0.1"
+			process.env.REDIS_PORT = "6379"
+			process.env.REDIS_NS = "test-ns"
+			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120ff"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "asd700qwe"
+			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationValueError)
 		})
 	})
 	describe("it should handle missing required fields", () => {
@@ -74,6 +123,8 @@ describe("loading configuration fields from environment should work", () => {
 			process.env.REDIS_PORT = "6379"
 			process.env.REDIS_NS = "test-ns"
 			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
 			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationError)
 		})
 		it("should throw an error when a required field is missing", () => {
@@ -87,6 +138,8 @@ describe("loading configuration fields from environment should work", () => {
 			process.env.REDIS_PORT = "6379"
 			process.env.REDIS_NS = "test-ns"
 			process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
 			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationError)
 		})
 		it("should throw an error when a required field is missing", () => {
@@ -100,6 +153,8 @@ describe("loading configuration fields from environment should work", () => {
 			// Process.env.REDIS_PORT = "6379"
 			// Process.env.REDIS_NS = "test-ns"
 			// Process.env.REDIS_QUEUE = "test-queue"
+			process.env.HEALTH_CHECK_INTERVAL = "120"
+			process.env.APPLY_DESIRED_STATE_INTERVAL = "700"
 			expect(() => getRedisConfigFromEnv()).toThrowError(InvalidConfigurationError)
 		})
 	})
