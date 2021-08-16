9c458b18fb4c5355e9f227c22b539acc0575eaa7
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2021-08-16 14:42:56 +0200

    refactor(redis-service): fix naming inconsitencies in tests

--- packages/redis/__tests__/redisService.spec.unit.ts
+++ packages/redis/__tests__/redisService.spec.unit.ts
@@ -2,8 +2,8 @@ import { RedisService } from "../src"
 describe("redis-service should pass all tests", () => {
 	beforeAll(() => {
 		process.env.TASKS_PER_CONTAINER = "5"
-		process.env.MAX_WORKER_CONTAINER = "10"
-		process.env.MIN_WORKER_CONTAINER = "5"
+		process.env.MAX_WORKER_CONTAINERS = "10"
+		process.env.MIN_WORKER_CONTAINERS = "5"
 		process.env.CONTAINER_LABEL = "redis-service-label"
 		process.env.CONTAINER_IMAGE = "bash"
 		process.env.CONTAINER_TAG = "latest"
