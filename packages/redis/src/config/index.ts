25ed3aa96c7ba94a582b38718c257eae81ec8906
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2021-08-16 09:28:02 +0200

    feat(redis-service): add function to load config from .env

--- packages/redis/src/config/index.ts
+++ packages/redis/src/config/index.ts
@@ -1,1 +1,48 @@
+import { IAutoScalerConfiguration } from "../../../auto-scaler/src/config"
+export const getAutoScalerConfigFromEnv = () : IAutoScalerConfiguration => {
+	let containerStartThreshold = 3
+	const envThreshold = process.env.TASKS_PER_CONTAINER
+	if (envThreshold) {
+		containerStartThreshold = parseInt(envThreshold)
+	}
+	let maxContainers = 50
+	const envMax = process.env.MAX_WORKER_CONTAINER
+	if (envMax) {
+		maxContainers = parseInt(envMax)
+	}
+	let minContainers = 50
+	const envMin = process.env.MIN_WORKER_CONTAINER
+	if (envMax) {
+		minContainers = parseInt(envMax)
+	}
+	let containerLabel = "foo-bar-123"
+	const envLabel = process.env.CONTAINER_LABEL
+	if (envLabel) {
+		containerLabel = envLabel
+	}
+	let imageId = "bash"
+	const envImage = process.env.CONTAINER_IMAGE
+	if (envImage) {
+		imageId = envImage
+	}
+	const tag = process.env.CONTAINER_TAG
+	const socketPath = process.env.DOCKER_SOCKET_PATH
+	const host = process.env.DOCKER_HOST
+	const port = process.env.DOCKER_PORT
+		? parseInt(process.env.DOCKER_PORT)
+		: undefined
+	return {
+		containerStartThreshold,
+		dockerConfig: {
+			containerLabel,
+			host,
+			imageId,
+			port,
+			socketPath,
+			tag
+		},
+		maxContainers,
+		minContainers
+	}
+}
 export * from "./interface"
