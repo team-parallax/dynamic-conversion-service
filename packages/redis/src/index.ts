672dcd6de9a847698a6f9c95d7d15ee9d10ad17e
Stefan Wintergerst <stefan.wintergerst@team-parallax.com>
2021-08-31 19:24:59 +0200

    chore(redis): very non-functional dockerization for redis

--- packages/redis/src/index.ts
+++ packages/redis/src/index.ts
@@ -1,2 +1,25 @@
+// It's important to import it here due to issue #87 (see above)
 import { Api } from "./api/rest"
-const app = new Api()
+import {
+	Container,
+	Scope
+} from "typescript-ioc"
+import { RedisService } from "./service"
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async (): Promise<void> => {
+	const redisService = new RedisService()
+	await redisService.initalize()
+	await redisService.checkHealth()
+	await redisService.applyState()
+	await redisService.start()
+	Container.bind(RedisService)
+		.factory(() => redisService)
+		.scope(Scope.Singleton)
+	const api = new Api()
+	// eslint-disable-next-line @typescript-eslint/no-misused-promises
+	process.on("SIGINT", async (): Promise<void> => {
+		await redisService.quit()
+		api.close()
+	})
+	api.listen()
+})()
