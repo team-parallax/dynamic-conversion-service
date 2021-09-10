01f3055bafcf65ed3c00d6d4fab1161a286c228e
Sven Patrick Meier <sven.patrick.meier@team-parallax.com>
2021-09-10 07:49:05 +0000

    Apply 2 suggestion(s) to 1 file(s)

--- packages/auto-scaler/src/docker/type.ts
+++ packages/auto-scaler/src/docker/type.ts
@@ -1,4 +1,6 @@
-/* https://github.com/moby/moby/blob/c858e496f670f3eb201f06a6b6cf5e2f4370687d/docs/api/v1.40.yaml#L4055 */
+/*
+* See https://github.com/moby/moby/blob/c858e496f670f3eb201f06a6b6cf5e2f4370687d/docs/api/v1.40.yaml#L4055
+*/
 export type TDockerContainerStatus =
  "created"
  | "running"
@@ -7,7 +9,9 @@ export type TDockerContainerStatus =
  | "removing"
  | "exited"
  | "dead"
- /* https://github.com/moby/moby/blob/c858e496f670f3eb201f06a6b6cf5e2f4370687d/docs/api/v1.40.yaml#L730 */
+ /*
+* See https://github.com/moby/moby/blob/c858e496f670f3eb201f06a6b6cf5e2f4370687d/docs/api/v1.40.yaml#L730
+*/
 export type TDockerHealthStatus =
   "none"
   | "starting"
